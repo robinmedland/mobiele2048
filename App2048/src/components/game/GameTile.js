027ba7e20ab538a906b7c81f95334e39ad749c09
STEFAN-DELL\Stefan <verresenstefan1996@gmail.com>
2018-06-21 12:03:36 +0200

    achtergrondkleuren robin

--- App2048/src/components/game/GameTile.js
+++ App2048/src/components/game/GameTile.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class GameTile extends Component {
+
     render() {
-		const { value } = this.props;
+        const { value } = this.props;
+        
         return (
-            <View style={styles.child}>
+            <View style={[styles.child, styles[`tile${value}`]]}>
                 <Text style={styles.tileText}>{value === 0 ? '' : value}</Text>
             </View>
         );
@@ -16,7 +18,6 @@ const styles = {
         width: '23%', 
         margin: '1%', 
         aspectRatio: 1,
-        backgroundColor: '#ADD8E6',
         justifyContent: 'center',
         alignItems: 'center'        
     },
@@ -24,6 +25,42 @@ const styles = {
         fontSize: 30,
         fontWeight: 'bold',
         
+    },
+    tile0: {
+        backgroundColor: '#c9ede5'
+    }, 
+    tile2: {
+        backgroundColor: '#7fc6b7'
+    },
+    tile4: {
+        backgroundColor: '#4a82a5'
+    },
+    tile8: {
+        backgroundColor: '#4456af'
+    },
+    tile16: {
+        backgroundColor: '#4927ad'
+    },
+    tile32: {
+        backgroundColor: '#511372'
+    },
+    tile64: {
+        backgroundColor: '#721247'
+    },
+    tile128: {
+        backgroundColor: '#d10a1b'
+    },
+    tile256: {
+        backgroundColor: '#48ea1c'
+    },
+    tile512: {
+        backgroundColor: '#48ea1c'
+    },
+    tile1024: {
+        backgroundColor: '#5e5407'
+    },
+    tile2048:{
+        backgroundColor: '#5e5407'
     }
 
 };
